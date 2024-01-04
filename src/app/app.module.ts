4633dfc3449afd30b2bb646cb24671a8c8513702
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2024-01-05 03:20:39 +0530

    Generate Components for Signin and Signup
    Developer a form for signup and signin using bootstrap and applied some custom styling. Setup the routes to render appropriate pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SigninComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
